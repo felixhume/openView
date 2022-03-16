93904c9c60155b9fc1b1b01f94d489260b10894d
felixhume <50804992+felixhume@users.noreply.github.com>
2022-03-15 20:32:07 -0500

    added a little url-gettin feature

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ web = input("enter unblock website  >")
 web_ = "wget -output-document="+web+" "+web
 
 os.system(web_)
+
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
-    try:
-        httpd.serve_forever()
-    except Exception as issue:
-        print(issue)
-        exit()
+    httpd.serve_forever()
+    with open(web+"\index.html") as file:
+        for line in file:
+            urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
+            print(urls)
+            for url in urls:
+                os.system("wget -output-document="+web+"/"+url)
