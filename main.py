7058efbdaad6b945778e8c4e498bb98001db5a9a
felixhume <50804992+felixhume@users.noreply.github.com>
2022-03-16 14:51:38 -0500

    added logo

--- main.py
+++ main.py
@@ -3,6 +3,45 @@ import http.server
 import socketserver
 import re
 
+print("""
+                         __    _                                   
+                    _wr""        "-q__                             
+                 _dP                 9m_     
+               _#P                     9#_                         
+              d#@                       9#m                        
+             d##                         ###                       
+            J###                         ###L                      
+            {###K                       J###K                      
+            ]####K      ___aaa___      J####F                      
+        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+     _g##############mZ_         __g##############m_               
+   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+  a###""          ,Z"#####@" '######"\g          ""M##m            
+ J#@"             0L  "*##     ##@"  J#              *#K           
+ #"               `#    "_gmwgm_~    dF               `#_          
+7F                 "#_   ]#####F   _dK                 JE          
+]                    *m__ ##### __g@"                   F          
+                       "PJ#####LP"                                 
+ `                       0######_                      '           
+                       _0########_                                   
+     .               _d#####^#####m__              ,              
+      "*w_________am#####P"   ~9#####mw_________w*"                  
+          ""9@#####@M""           ""P@#####@M""                    
+
+
+
+
+                          ____   ____.__               
+  ____ ______   ____   ___\   \ /   /|__| ______  _  __
+ /  _ \\____ \_/ __ \ /    \   Y   / |  |/ __ \ \/ \/ /
+(  <_> )  |_> >  ___/|   |  \     /  |  \  ___/\     / 
+ \____/|   __/ \___  >___|  /\___/   |__|\___  >\/\_/  
+       |__|        \/     \/                 \/        
+
+(openView is open-source! get it at github.com/felixhume/openView)
+(thanks for using!)
+
+""")
 PORT = 8080
 web = input("enter unblock website  >")
 web_ = "wget -output-document="+web+" "+web
@@ -14,7 +53,7 @@ with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
     httpd.serve_forever()
     with open(web+"\index.html") as file:
         for line in file:
-            urls = re.findall('href=', line)
+            urls = re.findall('href=""', line)
             print(urls)
             for url in urls:
                 os.system("wget -output-document="+web+"/"+url)
