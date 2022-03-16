cd3cd508cec1357b1be50b083eb7d402e69447c9
felixhume <50804992+felixhume@users.noreply.github.com>
2022-03-15 20:09:53 -0500

    that yonked code was garbage. mild fix

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ web_ = "wget "+web
 
 os.system(web_)
 Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
     httpd.serve_forever()
