c234551cab348a661aefc657d54dec88b1ff011a
felixhume <50804992+felixhume@users.noreply.github.com>
2022-03-15 20:07:18 -0500

    added web server...
    yoinked some web server code from online... its all coming together

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
-
+import http.server
+import socketserver
+PORT = 8080
 web = input("enter unblock website  >")
 web_ = "wget "+web
 
 os.system(web_)
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
