6d9257f1d1752ed7c0d2e749d057e56a0f91bc3b
felixhume <felixhume02@gmail.com>
2022-11-16 17:48:54 -0600

    updates, unfinished url system

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ print("openView is open-source! get it at github.com/felixhume/openView")
 PORT = 8080
 web = input("enter unblock website  >")
 web_ = "wget "+web
-
+urltype = 0
+#0: unknown
+#1: https://
+#2: /css/general.css
 os.system(web_)
 
 Handler = http.server.SimpleHTTPRequestHandler
@@ -15,7 +18,21 @@ with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
     httpd.serve_forever()
     with open("index.html") as file:
         for line in file:
-            urls_hs = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
-            print(urls_hs)
-            for url in urls_hs:
+            urls_https = re.findall("https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", line)
+            print(urls_https)
+            for url in urls_https:
+                if url[:8] == "https://":
+                    urltype = 1
+                elif url[1] == "/":
+                    urltype = 2
+                else:
+                    urltype = 0
+                print(urltype, "type")
+                #download the extra files
+                if urltype == 2:#need to create folders
+                    os.system("wget "+url)
+                elif urltype == 1:
+                    os.system("wget "+url)
                 os.system("wget "+url)
+
+
